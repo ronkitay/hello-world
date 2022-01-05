f3ead94161f62ac3adc5574ca6420c7b57bbbe5a
Ron Kitay <ronk@qwilt.com>
2022-01-05 16:22:43 +0200

    some refactoring

--- node/src/tttgame/tic-tac-toe.ts
+++ node/src/tttgame/tic-tac-toe.ts
@@ -13,30 +13,6 @@ export class Game {
         ['E', 'E', 'E']
     ]
 
-    gameOver(): boolean {
-        return this.allCellsMatch(this.board[0][0], this.board[0][1], this.board[0][2]) ||
-            this.allCellsMatch(this.board[1][0], this.board[1][1], this.board[1][2]) ||
-            this.allCellsMatch(this.board[2][0], this.board[2][1], this.board[2][2]) ||
-            this.allCellsMatch(this.board[0][0], this.board[1][0], this.board[2][0]) ||
-            this.allCellsMatch(this.board[0][1], this.board[1][1], this.board[2][1]) ||
-            this.allCellsMatch(this.board[0][2], this.board[1][2], this.board[2][2]) ||
-            this.allCellsMatch(this.board[0][0], this.board[1][1], this.board[2][2]) ||
-            this.allCellsMatch(this.board[0][2], this.board[1][1], this.board[2][0]) ||
-            this.currentTurn > 8;
-    }
-
-    allCellsMatch(cell1: string, cell2: string , cell3: string): boolean {
-        return cell1 == cell2 && cell2 == cell3 && cell1 != 'E';
-    }
-
-    printBoard(board: string[][]): void {
-        console.log("");
-        console.log(`${board[0][0]}  ${board[0][1]}  ${board[0][2]}    ===>    1  2  3`)
-        console.log(`${board[1][0]}  ${board[1][1]}  ${board[1][2]}    ===>    4  5  6`)
-        console.log(`${board[2][0]}  ${board[2][1]}  ${board[2][2]}    ===>    7  8  9`)
-        console.log("");
-    }
-
     play(): void {
         let rl = readline.createInterface({input, output});
 
@@ -45,7 +21,7 @@ export class Game {
 
     private runGame(rl: Interface) {
         if (!this.gameOver()) {
-            this.printBoard(this.board);
+            this.printBoard();
             rl.question('Whats your move? (1-9) ', (move) => {
                 let moveNumeric = parseInt(move)-1;
                 if (isNaN(moveNumeric) || moveNumeric < 0 || moveNumeric > 8) {
@@ -55,7 +31,7 @@ export class Game {
                 else {
                     let row = Math.floor(moveNumeric / 3);
                     let column = moveNumeric % 3;
-                    if (this.board[row][column] != 'E') {
+                    if (this.board[row][column] !== 'E') {
                         console.log('Invalid move - cell already occupied!');
                         setImmediate(() => this.runGame(rl));
                     } else {
@@ -68,9 +44,33 @@ export class Game {
         }
         else {
             console.log("Game finished - final board is:");
-            this.printBoard(this.board);
+            this.printBoard();
             process.exit(0);
         }
+    }
+
+    private gameOver(): boolean {
+        return this.allCellsMatch(this.board[0][0], this.board[0][1], this.board[0][2]) ||
+            this.allCellsMatch(this.board[1][0], this.board[1][1], this.board[1][2]) ||
+            this.allCellsMatch(this.board[2][0], this.board[2][1], this.board[2][2]) ||
+            this.allCellsMatch(this.board[0][0], this.board[1][0], this.board[2][0]) ||
+            this.allCellsMatch(this.board[0][1], this.board[1][1], this.board[2][1]) ||
+            this.allCellsMatch(this.board[0][2], this.board[1][2], this.board[2][2]) ||
+            this.allCellsMatch(this.board[0][0], this.board[1][1], this.board[2][2]) ||
+            this.allCellsMatch(this.board[0][2], this.board[1][1], this.board[2][0]) ||
+            this.currentTurn > 8;
+    }
+
+    private allCellsMatch(cell1: string, cell2: string , cell3: string): boolean {
+        return cell1 === cell2 && cell2 === cell3 && cell1 !== 'E';
+    }
+
+    private printBoard(): void {
+        console.log("");
+        console.log(`${this.board[0][0]}  ${this.board[0][1]}  ${this.board[0][2]}    ===>    1  2  3`)
+        console.log(`${this.board[1][0]}  ${this.board[1][1]}  ${this.board[1][2]}    ===>    4  5  6`)
+        console.log(`${this.board[2][0]}  ${this.board[2][1]}  ${this.board[2][2]}    ===>    7  8  9`)
+        console.log("");
     }
 }
 
