b154f6ba4c675fabd23b086bfbf13bd32d2fe2ad
Ron Kitay <ronk@qwilt.com>
2022-01-23 19:54:18 +0200

    add all CRUD operaitions

--- node-course/lesson-2/exercise-2/src/car/car.controller.ts
+++ node-course/lesson-2/exercise-2/src/car/car.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { car } from '@prisma/client';
 import { CarService } from './car.service';
 
@@ -9,5 +9,20 @@ export class CarController {
     @Get()
     async getCars(): Promise<car[]> {
         return await this.service.getCars();
+    }
+
+    @Post()
+    async addCar(@Body() newCar: car): Promise<String> {
+        return await this.service.addCar(newCar);
+    }
+
+    @Put(":id")
+    async updateCar(@Param("id") carId: string, @Body() newCar: car): Promise<String> {
+        return await this.service.updateCar(carId, newCar);
+    }
+
+    @Delete(":id")
+    async deleteCar(@Param("id") carId: string): Promise<String> {
+        return await this.service.deleteCar(carId);
     }
 }
--- node-course/lesson-2/exercise-2/src/car/car.service.ts
+++ node-course/lesson-2/exercise-2/src/car/car.service.ts
@@ -3,10 +3,46 @@ import { car, PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class CarService {
-    
+
     async getCars(): Promise<car[]> {
         const prisma = new PrismaClient();
         return prisma.car.findMany();
     }
+    
+    async addCar(newCar: car): Promise<String> {
+        console.log(`About to add ${newCar}`);
+
+        const prisma = new PrismaClient();
+        await prisma.car.create({
+            data: newCar
+        })
+
+        return "Created";
+    }
+
+    async updateCar(carId: string, newCar: car): Promise<String> {
+        const prisma = new PrismaClient();
+        await prisma.car.update({
+            where: {
+                id : carId
+            },
+            data: newCar
+        });
+
+        return "Updated";
+
+    }
+    
+    async deleteCar(carId: string): Promise<String> {
+        const prisma = new PrismaClient();
+        await prisma.car.delete({
+            where: {
+                id : carId
+            }
+        });
+        return "Deleted";
+
+    }
+
 
 }
