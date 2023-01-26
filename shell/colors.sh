f99639235891c848800ed996cf71de93927a8838
Ron Kitay <ronk@qwilt.com>
2023-01-26 17:09:35 +0200

    add colors.sh

---
+++ shell/colors.sh
@@ -0,0 +1,8 @@
+export NORMAL=$(tput sgr0)
+export BRIGHT=$(tput bold)
+export RED=$(tput setaf 1)
+export GREEN=$(tput setaf 2)
+export YELLOW=$(tput setaf 3)
+export BLUE=$(tput setaf 4)
+export CYAN=$(tput setaf 6)
+export WHITE=$(tput setaf 7)
