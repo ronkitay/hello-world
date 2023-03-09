887bb6207a68e5be5b68e67f49ecd761d2125222
Ron Kitay <ronk@qwilt.com>
2023-03-09 16:06:22 +0200

    add bash example for checking a directory exists and fix the example for file existance

--- shell/conditions/check_file_or_direcory.sh
+++ shell/conditions/check_file_or_direcory.sh
@@ -2,5 +2,10 @@
 # Exactly 0 arguments
 if [ ! -f "$FILE" ]
 then
-    echo "File $FILE does not exist"
+    echo "$FILE does not exist or is not a file"
+fi
+
+if [ ! -d "$DIR" ]
+then
+    echo "$DIR does not exist or is not a directory"
 fi
