6bb9914757d3fa897468b1a349370a86c3658085
Ron Kitay <ronk@qwilt.com>
2023-09-07 10:02:16 +0300

    add basic arduino example

---
+++ arduino/basics/blink/basics-blink-port13/basics-blink-port13.ino
@@ -0,0 +1,17 @@
+void setup() {
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13, HIGH);
+  delay(300);
+
+  digitalWrite(13, LOW);
+  delay(200);
+
+  digitalWrite(13, HIGH);
+  delay(700);
+
+  digitalWrite(13, LOW);
+  delay(200);
+}
